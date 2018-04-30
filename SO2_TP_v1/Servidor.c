1e901da57c9541942b48807941caca20b761da8e
Franciscorp <franciscorp1994@hotmail.com>
2018-04-30 02:26:45 +0100

    Thread Temporizador
    faz sleep de 20 a 30 dentro da thread

--- SO2_TP_v1/Servidor.c
+++ SO2_TP_v1/Servidor.c
@@ -3,6 +3,9 @@
 
 int _tmain(int argc, TCHAR * argv[]) {
 
+	HANDLE temporizador = NULL;
+	DWORD WINAPI ThreadTemporizador(LPVOID param);
+
 	SYSTEMTIME tempoSistema;
 	FILETIME tempoFicheiro;
 	ULARGE_INTEGER tempoInicio;
@@ -24,6 +27,29 @@ int _tmain(int argc, TCHAR * argv[]) {
 	_tprintf(TEXT("Bem vindo ao Space Invaders!\n"));
 
 	_tprintf(TEXT("\nTempo do Sistema:H = %02d M = %02d S = %02d ms = %02d \n"), tempoSistema.wHour, tempoSistema.wMinute, tempoSistema.wSecond, tempoSistema.wMilliseconds);
-
+	
+	temporizador = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadTemporizador, NULL, 0, &temporizador);
+	WaitForSingleObject(temporizador, INFINITE);
+	_tprintf(TEXT("\nVou libertar a thread, xau\n"));
+	//free(temporizador); era preciso caso fosse um ponteiro
 	return 0;
+}
+
+
+DWORD WINAPI ThreadTemporizador(LPVOID param) {
+	time_t tempo;
+	int i = 0, random;
+
+	srand((int)time(&tempo));
+	_tprintf(TEXT("\nThread de 20 a 30\n"));
+	while (i < 10) {
+		random = (rand() % 10) + 20;
+		random = random * 1000;
+		Sleep(random);
+		_tprintf(TEXT("\nCriei um powerup\nTempo = %d\n"), random);
+		i++;
+	}
+	_tprintf(TEXT("\nAcabei a thread, vou encerrar\n"));
+	return 0;
+
 }
