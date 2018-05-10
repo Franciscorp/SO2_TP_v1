2106af8563eca8343e28ecc524f34206fc9619c4
Franciscorp <franciscorp1994@hotmail.com>
2018-05-11 00:27:43 +0100

    dll atualizadas v0

--- DLL_TP/DLL.cpp
+++ DLL_TP/DLL.cpp
@@ -18,3 +18,27 @@ int UmaString(void) {
 int soma(int x, int y){
 	return x + y;
 }
+
+typedef struct {
+	int idJog;
+	TCHAR movimento;//inteiro ou tchar?
+}mensagem;
+
+
+	HANDLE hMap;
+	LARGE_INTEGER t;
+	//t.QuadPart = 1 * sizeof(mensagem);
+	mensagem *msg;
+
+
+	int escreveMensagensMemPart() {
+		hMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, sizeof(mensagem), NULL, TEXT("bufferMensagens"));
+
+		if (hMap == NULL) {
+			_tprintf(TEXT("Erro no FileMapping\n"));
+			return -1;
+		}
+		msg = (mensagem *)MapViewOfFile(hMap, FILE_MAP_WRITE, 0, 0, 10 * sizeof(mensagem));
+
+	}
+
